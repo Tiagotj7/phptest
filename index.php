53a6c361ebe5baa2f96ecc000e5eceb956224254
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 16:18:14 -0300

    site php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Forever</title>
+</head>
+<body>
+    
+<script>
+
+    alert('Ola!')
+
+</script>
+
+
+    <h1>Teste em PHP</h1>
+
+    <p>Seja bem vindo ao teste diario</p>
+    
+</body>
+</html>
