7b0531192a85eba7dd1020fdd3988ad31bd96343
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 16:22:24 -0300

    comentar

--- index.php
+++ index.php
@@ -17,6 +17,17 @@
     <h1>Teste em PHP</h1>
 
     <p>Seja bem vindo ao teste diario</p>
-    
+
+    <p>A Data e Hora são: <?php echo $dataHoraAtual;?></p>
+
+    Obtém data e hora atual!!!
+
+    <!--Obtém a data e hora atual-->
+
+<?php 
+//Horário e Data 
+$dataHoraAtual = date('Y-m-d H:i:s');
+?>
+
 </body>
 </html>
